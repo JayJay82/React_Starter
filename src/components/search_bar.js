98edd4ef0aae769ebd11ed7495ecc2f5ce88bb96
Costantino Cavallo <c.cavallo@uario.com>
2018-09-12 16:16:41 +0200

    Controlled Component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,11 +1,23 @@
 import React , {Component} from 'react';
 
 export default class SearchBar extends Component {
-   render() {
-       return <input onChange= { this.onInputChange } />
+    constructor(props) {
+        super(props);
+        this.state = { term : 'Starting Value'};
+    }
+    
+    onInputChange = (event) => {
+        this.setState({ term : event.target.value});
+     }
+   // Controller Component - quando il valore è settato dallo state
+     render() {
+       return (
+        <div>
+            <input
+            value = { this.state.term }
+            onChange= { this.onInputChange } />
+        </div>
+    );
    }
 
-   onInputChange = (event) => {
-      console.log(event.target.value);
-   }
 }
