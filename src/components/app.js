156b1f4e1557077c7f0aeeac0dcf820d36d0db09
Costantino Cavallo <c.cavallo@uario.com>
2018-09-12 16:42:28 +0200

    Videolist + Props

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import SearchBar from './search_bar';
 import YTSearch from 'youtube-api-search';
+import VideoList from './video_list';
 
 const search_url = "https://www.googleapis.com/youtube/v3/search?part=snippet&order=relevance&maxResults=10&q=";
 const api_key = "AIzaSyDYY7s68X-pS4ouLUvEKNP1mzKZxOe3MoQ";
 
-//Functional component - quando non viene gestito lo state
+
 export default class App extends Component {
   
   constructor(props) {
@@ -18,7 +19,10 @@ export default class App extends Component {
 
   render() {
     return (
-      <SearchBar />
+      <div>
+        <SearchBar />
+        <VideoList videos= {this.state.videos} />
+      </div>
     );
   }
 }
